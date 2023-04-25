f3da67dd191b4cf949efc110980adfb51ce4488b
Stephen Bennett <s.bennett87@gmail.com>
2023-04-24 21:21:35 -0400

    adding in initial author page

--- site/pages/blog/author/[author].tsx
+++ site/pages/blog/author/[author].tsx
@@ -13,8 +13,58 @@ type Props = {
 }
 
 export default function blog({ data, relatedEntries }: Props) {
-  console.log(data)
-  return <div className="block w-full"></div>
+  const author = data[0].fields
+  console.log(author)
+  console.log(relatedEntries)
+  return (
+    <div className="block w-full">
+      <div>{author.name}</div>
+      <div>{author.jobTitle}</div>
+      <div>{author.profilePhoto.fields.file.url}</div>
+      {/* <div>{author.bio.content}</div> */}
+
+      <section className="block w-full max-w-6xl m-auto">
+        <h2 className="text-3xl mt-20 mb-6">Related Posts</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {relatedEntries.items.map((item: any) => (
+            <div
+              key={item.sys.id}
+              className="block bg-white text-gray-800 shadow-lg"
+            >
+              <img
+                className="h-64 object-cover w-full"
+                src={item.fields.heroImage?.fields.file.url}
+                alt={data[0].fields.heroImage?.fields.description}
+              />
+              <div className="p-4">
+                <h3 className="text-3xl mt-2 font-semibold ">
+                  {item.fields.title}
+                </h3>
+                <Link
+                  className="
+                mt-4
+                py-2
+                px-4
+              bg-white
+              text-black
+                border-gray-500
+                border-2
+                inline-block
+                hover:bg-gray-900
+                hover:border-gray-900
+                hover:text-white
+                transition"
+                  href={`/blog/${item.fields.slug}`}
+                >
+                  Read More
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
+  )
 }
 
 export async function getServerSideProps(context: any) {
@@ -29,11 +79,15 @@ export async function getServerSideProps(context: any) {
     'fields.slug': context.params.author,
   })
 
+  const authorEntryId = entries.items[0].sys.id
+
   const relatedEntries = await client.getEntries({
     content_type: 'blogPost',
     limit: 3,
-    'fields.slug[ne]': context.params.blog,
+    'fields.author.sys.id': authorEntryId,
   })
+
+  console.log(relatedEntries)
 
   const data = entries.items
 
